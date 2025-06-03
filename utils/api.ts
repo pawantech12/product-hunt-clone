18a5585abf2fac73a8cbecb204f9d53f2a13a8ec
pawantech12 <pawankumavat042@gmail.com>
2025-06-03 10:39:41 +0530

    changed base url for backend

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Replace with real or mock API
+  baseURL: "https://product-hunt-clone-ten.vercel.app/api", // Replace with real or mock API
 });
 
 export default api;
